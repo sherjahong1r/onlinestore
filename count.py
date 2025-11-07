6b79b0ff0979ae7cd704bd91e42a7dac95190f4d
Sher_jahongir Tursunmurodov <sherjahongir77@gmail.com>
2025-11-07 08:39:20 -0800

    Add files via upload
    This is simple python  codes for calculation tasks!

---
+++ count.py
@@ -0,0 +1,50 @@
+"""
+Created on Mon Nov  3 06:15:00 2025
+Daromat va Zararni hisoblaymiz
+@author: Owner
+"""
+            
+            
+print("\n\nSizning xarajatlaringizni hisoblaymiz.")
+
+while True:
+    savol = input("\nBoshlash uchun start so'zini kiriting"
+                  "\nxarajat va zarar uchun son oldiga - qo'ying (-), "
+                  "\nTugashi uchun end deb yozing; Namuna: (start/end):"
+                  "\n\nNamunani kiriting >>> ").lower()
+
+    if savol == "start":
+        try:
+            umumiy_foyda = 0
+            while True:
+                daromat = int(input("\nHisobingizni kiriting: >>> "))
+                umumiy_foyda += daromat
+                
+
+                again = int(input("Yana son kiritasizmi? (1=Ha / 0=Yo'q): >>> "))
+                if again == 0:
+                    break
+
+            print(f"\nUmumiy xisob: {umumiy_foyda} ✅")
+            if umumiy_foyda > 0:
+                print(f"Siz {umumiy_foyda} 000 so'm foyda qilibsiz ")
+            if umumiy_foyda == 0:
+                print(f"Umumiy xisob {umumiy_foyda} so'm "
+                      "foyda ham zarar ham qilmabsiz")
+            if umumiy_foyda < 0:
+                print(f"siz {umumiy_foyda} 000 so'm zararga kiribsiz")
+        except ValueError:
+            print("❗ Siz faqat son kiritishingiz mumkin!")
+    
+    
+    elif savol == "end":
+        
+        print("Hisob tugadi ✅")
+        break
+
+    else:
+        print("Nimadir noto‘g‘ri! Qaytadan urinib ko‘ring.")
+
+            
+            
+            
